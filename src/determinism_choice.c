f462257aed375927e33cc7a703d38469c79cf394
cesar colle <cesar.colle@gmail.com>
2016-09-27 08:55:21 +0200

    determinisn choice adding

--- src/determinism_choice.c
+++ src/determinism_choice.c
@@ -11,6 +11,16 @@
 //TODO : buld distance need;
 double distance_calculator(cell * c1, cell * c2){
   return sqrt(
-
+    pow(c1->x - c2.x, 2) + pow(c1->y - c2->y, 2)
   );
+}
+
+/**
+* Move function
+* choose the good direction for the player
+**/
+
+void move(cell * current, grid * map){
+
+
 }
