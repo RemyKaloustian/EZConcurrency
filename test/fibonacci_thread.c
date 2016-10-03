4fb8b37a084a2b53ddd3d885ae9674b8050677a5
cesar colle <cesar.colle@gmail.com>
2016-10-03 09:11:38 +0200

    avance report

--- test/fibonacci_thread.c
+++ test/fibonacci_thread.c
@@ -3,58 +3,67 @@
 #include <pthread.h>
 #include <time.h>
 
-#define N 30
+#define N 28
+static int data[N] = {100, 300, 75000, 15000,
+	 4000, 5000, 3300,
+		2700, 3000, 4400, 4200,
+		1800, 1900, 4900, 18000, 20000
+		, 1200, 40000, 12000, 4, 67,
+	20, 124, 89, 5600, 4000, 1200, 8000};
 
 long long int fibb(int n) {
-  //printf("N = %d\n", n);
-	long int fnow = 0, fnext = 1, tempf;
-	while(--n>0){
-		tempf = fnow + fnext;
-		fnow = fnext;
-		fnext = tempf;
-		}
-    //printf("is : %ld \n", fnext);
-		return fnext;
+	int fnow = 0, fnext = 1, tempf;
+	while(--n > 0){
+			tempf = fnow + fnext;
+			fnow = fnext;
+			fnext = tempf;
+	}
 }
+  //printf("N = %d\n", n);
+	void * run (void * ptr_data){
+		int nb =*((int * ) ptr_data);
+		for (size_t i = nb - (N / 4); i < nb; i++) {
+			time_t t1 = clock();
+			fibb(data[i]);
+			time_t t2 = clock();
+			printf("%f %d\n",
+			 (float) (t2 - t1)/ CLOCKS_PER_SEC,data[i]);
+		}
 
-void * run (void * ptr_data){
-    time_t t1 = clock();
-    fibb(*((int * ) ptr_data));
-    time_t t2 = clock();
-    printf("%f %d\n",
-     (float) (t2 - t1)/ CLOCKS_PER_SEC,*((int * ) ptr_data));
      return NULL;
 }
 
-pthread_t threads[N];
+pthread_t threads[4];
 
 int main(int argc, char const *argv[]) {
-  int data[N] = {100, 300, 8000, 15000,
-     4000, 5000, 3300, 2200,
-      2700, 3000, 4400, 4200,
-      1800, 1900, 4900, 18000, 20000
-      , 1200, 40000, 12000, 4, 67, 9000,
-    20, 124, 89, 5600, 4000, 1200, 8000};
 	size_t i = 0;
-  for (; i < N/4 ; i++) {
-    pthread_create(&threads[i], NULL, run, &data[i]);
-  }
-	for (; i < 2*N/4 ; i++) {
-		pthread_create(&threads[i], NULL, run, &data[i]);
-	}
-
-	for (; i < 3*N/4 ; i++) {
-		pthread_create(&threads[i], NULL, run, &data[i]);
-	}
-
-	for (; i < N ; i++) {
-		pthread_create(&threads[i], NULL, run, &data[i]);
-	}
+  // for (; i < N/4 ; i++) {
+  //   pthread_create(&threads[i], NULL, run, &data[i]);
+  // }
+	// for (; i < 2*N/4 ; i++) {
+	// 	pthread_create(&threads[i], NULL, run, &data[i]);
+	// }
+	//
+	// for (; i < 3*N/4 ; i++) {
+	// 	pthread_create(&threads[i], NULL, run, &data[i]);
+	// }
+	//
+	// for (; i < N ; i++) {
+	// 	pthread_create(&threads[i], NULL, run, &data[i]);
+	// }
+	int b1 = N / 4;
+	int b2 = 2*N / 4;
+	int b3 = 3*N / 4;
+	int b4 = N;
+ 	pthread_create(&threads[0], NULL, run, &b1);
+	pthread_create(&threads[1], NULL, run, &b2);
+	pthread_create(&threads[2], NULL, run, &b3);
+	pthread_create(&threads[3], NULL, run, &b4);
 
   int rc = 0;
   int t = 0;
   void *  status;
-  for(t=0; t<N; t++) {
+  for(t=0; t<4; t++) {
        rc = pthread_join(threads[t], &status);
   }
   return 0;
